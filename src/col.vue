4afab64788beee897beb0687d0a655a934de7116
hu970804 <812510003@qq.com>
2018-09-09 09:43:06 +0800

    简单完成row col

---
+++ src/col.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'blackCol',
+        props: {
+            span: {
+                type: [Number, String]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .col {
+        height: 100px;
+        background-color: #ccc;
+        width: 50%;
+        border: 1px solid #223223;
+
+        $class-prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n / 24) * 100%;
+            }
+        }
+    }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="row">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    .row {
+        display: flex;
+    }
+</style>
