b5d8ec26bb45d92d289de4fa09db31a9963c27be
hu970804 <812510003@qq.com>
2018-09-11 21:18:34 +0800

    添加toast 测试用例

---
+++ src/layout/aside.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+    <transition name="slide">
+        <div class="aside" v-if="visible">
+            <slot></slot>
+            <a @click="visible=false">隐藏</a>
+        </div>
+    </transition>
+</template>
+
+<script>
+    export default {
+        name: 'blackAside',
+        data() {
+            return {
+                visible: true
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    .aside {
+        position: relative;
+        > a {
+            cursor: pointer;
+            position: absolute;
+            top: 0;
+            right: 0;
+        }
+    }
+    .slide-enter-active, .slide-leave-active {
+        transition: all .5s;
+    }
+    .slide-enter, .slide-leave-to {
+        margin-left: -200px;
+    }
+</style>
--- src/toast.vue
+++ src/toast.vue
@@ -18,12 +18,11 @@
         name: 'blackToast',
         props: {
             autoClose: {
-                type: Boolean,
-                default: true
-            },
-            autoCloseDelay: {
-                type: Number,
-                default: 3
+                type: [Boolean, Number],
+                default: 3,
+                validator(value) {
+                    return value === false || typeof  value === 'number';
+                }
             },
             closeButton: {
                 type: Object,
@@ -66,7 +65,7 @@
                 if(this.autoClose) {
                     setTimeout( () => {
                         this.close()
-                    }, this.autoCloseDelay*1000)
+                    }, this.autoClose*1000)
                 }
             },
             close() {
