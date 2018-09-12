adabaabe3e7fe73eb8912ffd2474574087dc2cdf
hu970804 <812510003@qq.com>
2018-09-12 16:15:34 +0800

    神奇的bug

---
+++ src/layout/footer.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+    <div class="foot">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss" scoped>
+    .foot {
+    }
+</style>
---
+++ src/layout/header.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+    <div class="header">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss" scoped>
+    .header {
+    }
+</style>
